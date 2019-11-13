8661374fd7245192865217e9a9a28eeefcb5d32b
zxq123456-teach <1060604915@qq.com>
2019-11-13 21:36:42 +0800

    黑马头条PC-主页模块-页面布局

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import ElementUI from 'element-ui'
+import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './styles/index.less'
 import axios from 'axios'
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 Vue.prototype.$axios = axios
-Vue.use(ElementUI)
+Vue.use(Element)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>
-    主页
-  </div>
+  <el-container>
+    <el-aside>左侧内容</el-aside>
+    <el-container>
+      <el-header>头部内容</el-header>
+      <el-main>中间内容</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
