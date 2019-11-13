72665711c3807b846764bd8c763e3ffb9009a5b2
zxq123456-teach <1060604915@qq.com>
2019-11-13 17:57:29 +0800

    黑马头条PC-初始化项目-页面分析-新建登录和主页

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,25 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+import Home from '../views/home' // 简写路径
+import Login from '../views/login' // 简写路径
 Vue.use(VueRouter)
 
-const routes = [{
-  path: '/',
-  name: 'home',
-  component: Home
-}
-  // {
-  //   path: '/about',
-  //   name: 'about',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
+const routes = [
+  {
+    path: '/',
+    redirect: '/home'
+  },
+  // 一级路由 主页
+  {
+    path: '/home',
+    name: 'home',
+    component: Home
+  },
+  {
+    // 一级路由 登录页
+    path: '/login',
+    component: Login
+  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/login/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      denglu
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
