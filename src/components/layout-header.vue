b7a1a0ec01d3b18c74ff382c3102d60c5e39c0d1
zxq123456-teach <1060604915@qq.com>
2019-11-14 16:54:16 +0800

    <template>

--- src/components/layout-header.vue
+++ src/components/layout-header.vue
@@ -1,17 +1,11 @@
 <template>
-  <!-- 头部组件 -->
   <el-row type='flex' justify='space-between' align='middle'>
-      <!-- 左侧 -->
       <el-col :span='6' class='left'>
-          <!-- 左侧 -->
           <i class='el-icon-s-fold'></i>
           <span>江苏传智播客教育科技股份有限公司</span>
       </el-col>
-      <!-- 右侧 -->
       <el-col :span='3' class='right'>
-          <!-- 头像 -->
           <img src="../assets/img/avatar.jpg" alt="">
-          <!-- 下拉菜单 -->
           <el-dropdown trigger='click'>
               <span style="cursor:pointer">猪猪女孩</span>
               <el-dropdown-menu slot='dropdown'>
