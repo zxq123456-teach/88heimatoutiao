1cbaedcebff1fae8a6671a6c42aa098b2a488d69
zxq123456-teach <1060604915@qq.com>
2019-11-16 17:52:38 +0800

    文章列表模块-数据分页

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -39,7 +39,7 @@
         <!-- 文章列表 -->
         <el-card class="box-card">
             <div slot="header" class="clearfix">
-                <span>共找到59806条符合条件的内容</span>
+                <span>共找到{{totalCount}}条符合条件的内容</span>
             </div>
             <el-table
             :data='articles'
@@ -80,6 +80,14 @@
                 </el-table-column>
             </el-table>
         </el-card>
+
+        <!-- 分页 -->
+        <el-pagination
+        background
+        layout='prev,pager,next'
+        :total='totalCount'
+        @current-change='onPageChange'>
+        </el-pagination>
     </div>
 </template>
 
@@ -118,14 +126,17 @@ export default {
           type: 'info',
           label: '已删除'
         }
-      ]
+      ],
+      // 总记录数
+      totalCount: 0
     }
   },
   created () {
-    this.loadArticles()
+    // 初始化的时候加载第一页数据
+    this.loadArticles(1)
   },
   methods: {
-    loadArticles () {
+    loadArticles (page = 1) {
       // 把token放到请求头中
       const token = window.localStorage.getItem('user-token')
 
@@ -135,12 +146,24 @@ export default {
         headers: {
           // 添加请求头
           Authorization: `Bearer ${token}`
+        },
+        // Query 参数使用 params 传递
+        params: {
+          page,
+          per_page: 10
         }
       }).then(res => {
+        // 更新文章列表数组
         this.articles = res.data.data.results
+        // 更新总记录数
+        this.totalCount = res.data.data.total_count
       }).catch(err => {
         console.log(err, '获取数据失败')
       })
+    },
+    onPageChange (page) {
+      // 请求加载指定页码的文章列表
+      this.loadArticles(page)
     }
   }
 }
