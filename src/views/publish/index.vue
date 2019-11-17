3da717494747f2e9d9e1b56321b924d9931c8015
zxq123456-teach <1060604915@qq.com>
2019-11-17 16:34:32 +0800

    文章发布-使用富文本编辑器

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -9,17 +9,27 @@
           <el-input v-model='article.title'></el-input>
         </el-form-item>
         <el-form-item label='内容'>
-          <el-input type='textarea' v-model='article.content'></el-input>
+          <!-- <el-input type='textarea' v-model='article.content'></el-input> -->
+          <!-- 富文本编辑器 -->
+          <quill-editor
+          v-model='article.content'
+          ref='myQuillEditor'
+          :options='editorOption'>
+          </quill-editor>
         </el-form-item>
         <el-form-item label='频道'>
-          <el-select v-model='article.channel_id' placeholder="请选择活动区域">
-            <el-option label='区域一' value='shanghai'></el-option>
-            <el-option label='区域二' value='beijing'></el-option>
+          <el-select v-model='article.channel_id' placeholder="请选择频道">
+            <el-option
+            :label='channel.name'
+            :value='channel.id'
+            v-for='channel in channels'
+            :key='channel.id'>
+            </el-option>
           </el-select>
         </el-form-item>
         <el-form-item>
-          <el-button type='primary' @click='onSubmit'>发表</el-button>
-          <el-button>存入草稿</el-button>
+          <el-button type='primary' @click='onSubmit(false)'>发表</el-button>
+          <el-button @click='onSubmit(true)'>存入草稿</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -27,24 +37,64 @@
 </template>
 
 <script>
+// 加载富文本编辑器
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
+// 加载富文本编辑器的核心组件
+import { quillEditor } from 'vue-quill-editor'
 export default {
   name: 'PublishArticle',
+  components: {
+    // 注册局部组件
+    quillEditor
+  },
   data () {
     return {
       article: {
-        title: '',
-        content: '',
+        title: '', // 文章标题
+        content: '', // 文章内容
         cover: {
-          type: 0,
-          images: []
+          type: 0, // 封面类型 -1自动 0无图  1：1张  3:3张
+          images: [] // 图片 无图为空数组
         },
         channel_id: ''
-      }
+      },
+      channels: [],
+      editorOption: {} // 富文本编辑器的配置选项对象
     }
   },
+  created () {
+    this.loadChennels()
+  },
   methods: {
-    onSubmit () {
-      console.log('submit')
+    onSubmit (draft) {
+      this.$axios({
+        method: 'POST',
+        url: '/articles',
+        headers: {
+          Authorization: `Bearer ${window.localStorage.getItem('user-token')}`
+        },
+        params: {
+          draft
+        },
+        data: this.article
+      }).then(res => {
+        console.log(res)
+      }).catch(err => {
+        console.log(err, '保存失败')
+      })
+    },
+    loadChennels () {
+      this.$axios({
+        method: 'GET',
+        url: '/channels'
+      }).then(res => {
+        this.channels = res.data.data.channels
+      }).catch(err => {
+        console.log(err, '获取数据失败')
+      })
     }
   }
 }
