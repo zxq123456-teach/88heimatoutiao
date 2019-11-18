96d5830efe8b06f11cc148c84653f5697532a83f
zxq123456-teach <1060604915@qq.com>
2019-11-18 20:23:07 +0800

    评论列表-展示文章评论列表

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -6,21 +6,37 @@
         </div>
 
         <el-table
-        :data='tableData'
+        :data='articles'
         style='width:100%'>
             <el-table-column
-            prop='date'
-            label='日期'
+            prop='title'
+            label='标题'
             width='180'>
             </el-table-column>
             <el-table-column
-            prop='name'
-            label='姓名'
-            width='180'>
+            prop='total_comment_count'
+            label='总评论数'>
             </el-table-column>
             <el-table-column
-            prop='address'
-            label='地址'>
+            prop='fans_comment_count'
+            label='粉丝评论数据'>
+            </el-table-column>
+            <el-table-column
+            label='评论状态'
+            width='180'>
+            <template slot-scope="scope">
+                <el-switch
+                v-model='scope.row.comment_status'
+                active-color='#13ce66'
+                inactive-color="#ff4949">
+                </el-switch>
+            </template>
+            </el-table-column>
+            <el-table-column
+            label='操作'>
+                <template>
+                    <el-button type='primary'>修改</el-button>
+                </template>
             </el-table-column>
         </el-table>
       </el-card>
@@ -34,28 +50,7 @@ export default {
   props: {},
   data () {
     return {
-      tableData: [
-        {
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路1518弄'
-        },
-        {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路1517弄'
-        },
-        {
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路1519弄'
-        },
-        {
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路1516弄'
-        }
-      ]
+      articles: [] // 文章列表（文章的评论数据字段）
     }
   },
   computed: {},
@@ -63,10 +58,22 @@ export default {
 
   },
   created () {
-
+    this.loadArticles()
   },
   methods: {
-
+    loadArticles () {
+      this.$axios({
+        method: 'GET',
+        url: '/articles',
+        params: {
+          response_type: 'comment'
+        }
+      }).then(res => {
+        this.articles = res.data.data.results
+      }).catch(err => {
+        console.log(err, '获取数据失败')
+      })
+    }
   }
 }
 
