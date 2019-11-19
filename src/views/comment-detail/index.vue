ee6bc8f9c48b5074a93f082ecb3337f48265a50a
zxq123456-teach <1060604915@qq.com>
2019-11-19 19:29:47 +0800

    评论管理-展示评论列表

--- src/views/comment-detail/index.vue
+++ src/views/comment-detail/index.vue
@@ -1,5 +1,65 @@
 <template>
-  <div>评论列表</div>
+  <div>
+      <el-card class='box-card'>
+          <div slot="header" class='clearfix'>
+              <span>评论详情列表</span>
+              <el-button style="float:right; padding:3px 0" type="text">操作按钮</el-button>
+          </div>
+
+          <el-table
+          :data='comments'
+          style='width:100%'>
+              <el-table-column
+              label='头像'
+              width='180'>
+                  <template slot-scope="scope">
+                      <img :src="scope.row.aut_photo" width='50'>
+                  </template>
+              </el-table-column>
+              <el-table-column
+              prop='content'
+              label='评论内容'
+              width='180'>
+              </el-table-column>
+              <el-table-column
+              prop='name'
+              label='点赞'
+              width="180">
+                  <template slot-scope="scope">
+                    {{ scope.row.is_liking === 1 ? '已赞' : '没有赞' }}</template>
+              </el-table-column>
+              <el-table-column
+              prop='like_count'
+              label='点赞数量'
+              width="180">
+              </el-table-column>
+              <el-table-column
+              prop='pubdate'
+              label='评论日期'
+              width="180">
+                   <template slot-scope="scope">
+                       {{ scope.row.pubdate | dateFormat('YYYY-MM-DD') }}</template>
+              </el-table-column>
+               <el-table-column
+                  label="是否推荐"
+                  width="180">
+                    <template slot-scope="scope">
+                       <el-switch
+                        v-model="scope.row.is_top"
+                        active-color="#13ce66"
+                        inactive-color="#ff4949"
+                        @change="onTop(scope.row)">
+                       </el-switch>
+                    </template>
+                </el-table-column>
+                <el-table-column
+                prop="reply_count"
+                label="回复数量"
+                width="180">
+                </el-table-column>
+          </el-table>
+      </el-card>
+  </div>
 </template>
 
 <script>
@@ -8,13 +68,35 @@ export default {
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      comments: []
+    }
   },
   computed: {},
   watch: {
   },
-  created () {},
-  methods: {}
+  created () {
+    this.loadComments()
+  },
+  methods: {
+    loadComments () {
+      this.$axios({
+        methos: 'GET',
+        url: '/comments',
+        params: {
+          type: 'a',
+          source: this.articled
+        }
+      }).then(res => {
+        const comments = res.data.data.results
+
+        this.comments = comments
+      }).catch(err => {
+        console.log(err)
+        this.$message.error('获取数据失败')
+      })
+    }
+  }
 }
 </script>
 
