5892c7f94409501f92fad771fd9a0606d0ae9422
zxq123456-teach <1060604915@qq.com>
2019-11-21 20:50:38 +0800

    发布文章-使用对话框

--- src/views/publish/components/upload-image.vue
+++ src/views/publish/components/upload-image.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>上传图片组件</div>
+  <div class="upload-image">
+    <div class="preview" @click="centerDialogVisible=true">
+      <!-- <img src="" class="avatar"> -->
+      <i class="el-icon-plus avatar-uploader-icon"></i>
+    </div>
+    <!--
+      visible 控制对话框的显示和隐藏
+     -->
+    <el-dialog
+      title="请选择文章封面图片"
+      :visible.sync="centerDialogVisible"
+      width="30%"
+      center>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="centerDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="centerDialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
@@ -8,7 +26,9 @@ export default {
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      centerDialogVisible: false
+    }
   },
   computed: {},
   watch: {
@@ -18,6 +38,31 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.upload-image {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
 
+.upload-image .el-upload:hover {
+  border-color: #409EFF;
+}
+
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
 </style>
