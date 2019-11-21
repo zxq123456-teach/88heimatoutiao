c5acde0ee21996b2daada4dd80d2a8e204a4d12a
zxq123456-teach <1060604915@qq.com>
2019-11-21 19:55:58 +0800

    账户设置-修改信息及头像上传

---
+++ src/utils/event-bus.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+
+// 该模块导出一个新建的空的 Vue 实例
+export default new Vue()
--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -1,23 +1,27 @@
 <template>
   <div>
-    <el-form ref="form" :model="form" label-width="80px">
+    <el-form ref="form" :model="user" label-width="80px">
       <el-form-item label="用户头像">
         <el-upload
           class="avatar-uploader"
           action="https://jsonplaceholder.typicode.com/posts/"
-          :show-file-list="false">
-          <img width="100" src="http://toutiao.meiduo.site/FuLZH0vzKbdXi-On88NArXegOjXk" class="avatar">
+          :show-file-list="false"
+          :http-request="onUpload">
+          <img v-if="user.photo" width="100" :src='user.photo' class="avatar">
           <!-- <i class="el-icon-plus avatar-uploader-icon"></i> -->
         </el-upload>
       </el-form-item>
       <el-form-item label="用户昵称">
-        <el-input v-model="form.name"></el-input>
+        <el-input v-model="user.name"></el-input>
       </el-form-item>
       <el-form-item label="个人介绍">
-        <el-input type="textarea" v-model="form.name"></el-input>
+        <el-input type="textarea" v-model="user.intro"></el-input>
       </el-form-item>
       <el-form-item label="用户邮箱">
-        <el-input v-model="form.name"></el-input>
+        <el-input v-model="user.email"></el-input>
+      </el-form-item>
+      <el-form-item label="手机号">
+        <el-input v-model="user.mobile"></el-input>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="onSubmit">保存修改</el-button>
@@ -27,34 +31,119 @@
 </template>
 
 <script>
+import eventBus from '@/utils/event-bus.js'
+
 export default {
   name: 'AccountIndex',
   components: {},
   props: {},
   data () {
     return {
-      form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
+      user: {
+        email: '', // 邮箱
+        intro: '', // 简介
+        mobile: '', // 手机号
+        name: '', // 昵称
+        photo: '' // 头像
       }
     }
   },
   computed: {},
   watch: {
   },
-  created () {},
+  created () {
+    this.loadUserProfile()
+  },
   methods: {
     onSubmit () {
-      console.log('保存')
+      // const name = this.user.name
+      // const email = this.user.email
+      // const intro = this.user.intro
+
+      // 对象解构：等价于上面的三句代码
+      const { name, email, intro } = this.user
+
+      this.$axios({
+        method: 'PATCH',
+        url: '/user/profile',
+        data: {
+          // name: this.user.name,
+          // email: this.user.email,
+          // intro: this.user.intro
+          name,
+          email,
+          intro
+        }
+      }).then(res => {
+        this.$message({
+          type: 'success',
+          message: '修改成功'
+        })
+      }).catch(err => {
+        console.log(err)
+        this.$message.error('修改失败')
+      })
+    },
+    loadUserProfile () {
+      this.$axios({
+        method: 'GET',
+        url: '/user/profile'
+      }).then(res => {
+        // res.date undefined
+        // undefined.data 报错了
+        this.user = res.data.data
+      }).catch(err => {
+        console.log(err)
+        this.$message.error('获取数据失败')
+      })
+    },
+    onUpload (config) {
+      const fd = new FormData()
+      fd.append('photo', config.file)
+      this.$axios({
+        method: 'PATCH',
+        url: '/user/photo',
+        data: fd
+      }).then(res => {
+        // 更新图片地址
+        this.user.photo = res.data.data.photo
+
+        // 发布通知头部组件更新用户信息
+        eventBus.$emit('update-user', this.user)
+      }).catch(err => {
+        console.log(err)
+        this.$message.error('上传失败')
+      })
     }
   }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+
+.avatar-uploader .el-upload:hover {
+  border-color: #409EFF;
+}
+
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
+</style>
